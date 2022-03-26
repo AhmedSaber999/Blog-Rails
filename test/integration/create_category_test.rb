b0d86faba0bdb4b2174acfe5fc5f7164e224b637
Ahmed saber <A7mdsaber99@gmail.com>
2022-03-26 15:27:03 +0200

    set admin validations

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
-class CreateCategoryTest < ActionDispatch::IntegrationTest
+class CreateCategoryTest < ActionDispatch::IntegrationTest  
+  setup do
+    @admin_user = User.create(username: "johndoe", email: "johndoe@example.com",
+                              password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
+
   test 'get new category form and create category' do
     get '/categories/new'
     assert_response :success
@@ -13,7 +19,6 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_match 'Sports', response.body
   end
 
-  
   test 'get new category form and inject invalid category submission' do
     get '/categories/new'
     assert_response :success
